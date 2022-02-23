28dbd9fcc08e9295302e216b80c81de6de3a2377
lqqojin <lqqojin@gmail.com>
2022-02-23 20:51:58 +0900

    [learn-typescript]_첫 번째 프로젝트 - 할 일 관리 애플리케이션

---
+++ 02-class-note/1_type-basic.ts
@@ -0,0 +1,31 @@
+// JS 문자열 선언
+// var str = 'hello';
+
+//  TS 문자열 선언
+let str: string = 'hello';
+
+// TS 숫자 선언
+let num: number = 10;
+
+// TS 배열
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+let items: number[] = [4, 5, 6];
+
+// TS 튜플
+// 튜플: 배열의 각각 인덱스에 타입이 정해져 있다.
+let address: [string, number] = ['gangnam', 100];
+
+// TS 객체
+let obj: object = {};
+// let person: object = {
+//     name: 'capt',
+//     age: 100
+// };
+let person: { name: string, age: number } = {
+    name: 'thor',
+    age: 1000,
+}
+
+// TS 진위값
+let show: boolean = true;
